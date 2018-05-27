36b9abbfc872855b72c60ac76c98a4ee3ed758c7
Felipe Marques <fmarques.ds@gmail.com>
2018-05-26 21:32:31 -0300

    Adicionada a criacao de Sessao pelo site

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,11 +2,21 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
+	@ManyToOne
 	private Sala sala;
+	@ManyToOne
 	private Filme filme;
 	
 	public Sessao(LocalTime horario, Sala sala, Filme filme) {
@@ -16,8 +26,45 @@ public class Sessao {
 		this.filme = filme;
 	}
 	
+	/** @deprecated Hibernate */
+	public Sessao() {
+		
+	}
+	
 	public LocalTime getHorarioTermino() {
 		return this.horario.plusMinutes(this.filme.getDuracao().toMinutes());
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
+	public Filme getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Filme filme) {
+		this.filme = filme;
 	}
 
 }
